e6e105e73dd68797ecad13e48aaf127dac08b4c4
momentino <filippomomente@protonmail.com>
2024-11-30 19:44:49 +0100

    fix: reconnected submodule

--- benchmarks/lm-evaluation-harness/lm_eval/tasks/glue_diagnostics/utils.py
+++
@@ -1,3 +0,0 @@
-def doc_to_target(doc) -> int:
-    map = {"entailment":0, "contradiction":1, "neutral":2}
-    return map[doc['Label']]
--- benchmarks/lm-evaluation-harness/test.py
+++
@@ -1,7 +0,0 @@
-import datasets
-from datasets import load_dataset
-
-dataset = load_dataset('clembench-playpen/lmentry',"ends_with_letter")
-
-print(dataset)
-
